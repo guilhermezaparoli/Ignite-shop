d5a2529eef5409ffd678acfb25b2f1c02f547eb3
Guilherme <guilhermezapas@gmail.com>
2024-05-12 17:59:33 -0300

    Create of home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,28 @@
+
+import { HomeContainer, Product } from "./styles/pages/home";
+import camiseta1 from "../assets/camisetas/1.png"
+import camiseta2 from "../assets/camisetas/2.png"
+import camiseta3 from "../assets/camisetas/3.png"
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div>Hello World!</div>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} alt="" width={520} height={480} />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta2} alt="" width={520} height={480} />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
 
