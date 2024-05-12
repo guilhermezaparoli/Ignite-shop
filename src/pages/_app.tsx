04eb879fe7851933420050e5ca197dfb64f8af0f
Guilherme <guilhermezapas@gmail.com>
2024-05-12 15:56:44 -0300

    install and config stitches, config _document and create th header of the pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
+import { AppProps } from 'next/app';
+import { globalStyles } from './styles/global';
+import logoImg from '../assets/logo.svg';
+import { Container, Header } from './styles/pages/app';
+globalStyles();
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt='' />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
-
-export default MyApp
