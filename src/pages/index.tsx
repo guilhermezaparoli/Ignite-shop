78d6d8e8fc747e7627073a0d9786c46a226cf355
Guilherme <guilhermezapas@gmail.com>
2024-05-13 21:08:19 -0300

    adding keenSlider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,46 @@
 
 import { HomeContainer, Product } from "./styles/pages/home";
+import {useKeenSlider} from "keen-slider/react"
+import "keen-slider/keen-slider.min.css"
 import camiseta1 from "../assets/camisetas/1.png"
 import camiseta2 from "../assets/camisetas/2.png"
 import camiseta3 from "../assets/camisetas/3.png"
+import camiseta4 from "../assets/camisetas/4.png"
 import Image from "next/image";
 
-export default function Home() {
+export default function Home(props) {
+  const [sliderRef] = useKeenSlider<HTMLDivElement>({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+   
+  })
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={camiseta1} alt="" width={520} height={480} />
         <footer>
           <strong>Camiseta X</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={camiseta2} alt="" width={520} height={480} />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} alt="" width={520} height={480} />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta4} alt="" width={520} height={480} />
         <footer>
           <strong>Camiseta X</strong>
           <span>R$ 79,90</span>
@@ -27,3 +51,10 @@ export default function Home() {
 }
 
 
+export const getServerSideProps = () => {
+  return {
+    props: {
+      list: [1,2,3]
+    }
+  }
+}
--- src/pages/styles/pages/home.ts
+++ src/pages/styles/pages/home.ts
@@ -2,7 +2,7 @@ import { styled } from '..';
 
 export const HomeContainer = styled('main', {
   display: 'flex',
-  gap: '3rem',
+  // gap: '3rem',
   width: '100%',
   maxWidth: 'calc(100vw - ((100vw - 1180px) / 2))',
   marginLeft: 'auto',
@@ -12,15 +12,15 @@ export const HomeContainer = styled('main', {
 export const Product = styled('a', {
   background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
   borderRadius: 8,
-  padding: '0.25rem',
+  // padding: '0.25rem',
   cursor: 'pointer',
   position: 'relative',
+  overflow: 'hidden',
 
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  overflow: 'hidden',
-
+minWidth: 540,
   img: {
     objectFit: 'cover',
   },
@@ -56,7 +56,7 @@ export const Product = styled('a', {
 
   '&:hover': {
     footer: {
-      transiform: 'translateY(0%)',
+      transform: 'translateY(0%)',
       opacity: 1,
     },
   },
