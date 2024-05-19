2c06ac65530d2a03d6aab60f53e6cba13a5bb8c9
Guilherme <guilhermezapas@gmail.com>
2024-05-19 20:51:53 -0300

    Create modal from reactpopup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,17 +6,24 @@ import {
   BoxHeader,
   Container,
   ContainerCartIcon,
+  ContainerImage,
+  ContainerItems,
   ContentPopup,
+  FooterPopup,
   Header,
+  Item,
   StyledPopup,
 } from '../styles/pages/app';
 import Image from 'next/image';
 import BagCartIcon from '../assets/images/icon-bag.svg';
-import IconX from "../assets/images/x-icon.svg"
+import IconX from '../assets/images/x-icon.svg';
+import { useState } from 'react';
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const closeModal = () => setIsOpen(false);
   return (
     <>
       <Container>
@@ -24,30 +31,69 @@ export default function App({ Component, pageProps }: AppProps) {
           <BoxHeader>
             <Image src={logoImg} alt="" />
 
+            <ContainerCartIcon onClick={() => setIsOpen(true)}>
+              <AmountItensCart>
+                <p>1</p>
+              </AmountItensCart>
+              <Image src={BagCartIcon} alt="" />
+            </ContainerCartIcon>
+
             <StyledPopup
-              trigger={
-                <ContainerCartIcon>
-                  <AmountItensCart>
-                    <p>1</p>
-                  </AmountItensCart>
-                  <Image src={BagCartIcon} alt="" />
-                </ContainerCartIcon>
-              }
               modal
-              // nested
-              position={'right'}
+              nested
               closeOnEscape
+              open={isOpen}
+              onClose={closeModal}
+              position={'right center'}
             >
-              {(close) => (
+              {
                 <ContentPopup>
-                  <Image src={IconX} alt='' onClick={close}/>
-                  <h1>Sacola de compras</h1>
-
                   <div>
+                    <ContainerImage>
+                      <Image src={IconX} alt="" onClick={closeModal} />
+                    </ContainerImage>
+                    <h1>Sacola de compras</h1>
 
+                    <ContainerItems>
+                      <Item>
+                        <div>Imagem</div>
+                        <div>
+                          <p>Camiseta Beyond the Limits</p>
+                          <strong>R$ 79,90</strong>
+                          <a>Remover</a>
+                        </div>
+                      </Item>
+                      <Item>
+                        <div>Imagem</div>
+                        <div>
+                          <p>Camiseta Beyond the Limits</p>
+                          <strong>R$ 79,90</strong>
+                          <a>Remover</a>
+                        </div>
+                      </Item>
+                    </ContainerItems>
                   </div>
+
+                  <FooterPopup>
+                    <div>
+                      <p>Quantidade</p>
+                      <p>3 itens</p>
+                    </div>
+                    <div>
+                      <strong>Valor total</strong>
+                      <strong>R$ 270,00</strong>
+                    </div>
+
+                    <button
+                      onClick={() => {
+                        closeModal();
+                      }}
+                    >
+                      Finalizar compra
+                    </button>
+                  </FooterPopup>
                 </ContentPopup>
-              )}
+              }
             </StyledPopup>
           </BoxHeader>
         </Header>
--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -1,73 +1,169 @@
-import Popup from "reactjs-popup";
-import { styled } from "..";
+import Popup from 'reactjs-popup';
+import { styled } from '..';
 
-export const Container = styled("div", {
-    display: "flex",
-    flexDirection: 'column',
-    justifyContent: 'center',
-    minHeight:"100vh"
-})
+export const Container = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  minHeight: '100vh',
+});
 
-export const Header = styled("header", {
-    padding: '2rem 0',
-    width: '100%',
-    maxWidth: 1180,
-    margin: "0 auto"
-})
+export const Header = styled('header', {
+  padding: '2rem 0',
+  width: '100%',
+  maxWidth: 1180,
+  margin: '0 auto',
+});
 
-export const BoxHeader = styled("div", {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  
-})
+export const BoxHeader = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+});
 
-export const ContainerCartIcon = styled("div", {
-    alignItems: 'center',
-    borderRadius: 6,
-    backgroundColor: '$gray700',
-    width: 48,
-    height: 48,
-    position: 'relative',
-    cursor: 'pointer',
+export const ContainerCartIcon = styled('div', {
+  alignItems: 'center',
+  borderRadius: 6,
+  backgroundColor: '$gray700',
+  width: 48,
+  height: 48,
+  position: 'relative',
+  cursor: 'pointer',
 
-    display: 'flex',
-    justifyContent: 'center',
+  display: 'flex',
+  justifyContent: 'center',
 
-    p: {
-        color: '$white',
-        fontSize: 14,
-        fontWeight: 'bold',
-        lineHeight: 1.6,
-    }
-})
+  p: {
+    color: '$white',
+    fontSize: 14,
+    fontWeight: 'bold',
+    lineHeight: 1.6,
+  },
+});
 
+export const AmountItensCart = styled('div', {
+  width: 24,
 
-export const AmountItensCart = styled("div", {
-    width: 24,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  borderRadius: 999,
+  border: '3px solid $gray900',
+  backgroundColor: '$green500',
 
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 999,
-    border: '3px solid $gray900',
-    backgroundColor: "$green500",
-
-    position: 'absolute',
-    top: -8,
-    right: -8
-})
+  position: 'absolute',
+  top: -8,
+  right: -8,
+});
 
 export const StyledPopup = styled(Popup, {
-
-   '&-content': {
-    backgroundColor: 'red',
+  '&-content': {
+    backgroundColor: '$gray800',
     height: '100%',
     padding: '3rem',
-    position: 'absolute',
-}
-})
-
+    marginRight: '0 !important',
+  },
+});
 
 export const ContentPopup = styled('div', {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    height: '90vh',
+
+  h1: {
+    fontSize: '1.25rem',
+    fontWeight: 'bold',
+    lineHeight: 1.6,
+    marginTop: 24,
+    marginBottom: 32,
+  },
+});
+
+export const ContainerImage = styled('div', {
+    display: 'flex',
+    alignItems:'center',
+    justifyContent: 'flex-end',
+
+    img: {
+        cursor: 'pointer',
+        transition: 'filter 0.3s',
+
+        '&:hover': {
+            filter: 'brightness(1.2)'
+        }
+    }
+
 })
+
+export const ContainerItems = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 24,
+});
+
+export const Item = styled('div', {
+  display: 'flex',
+  gap: 20,
+
+  div: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 8,
+
+    p: {
+      fontSize: '1.125rem',
+      lineHeigth: 1.6,
+    },
+    strong: {
+      fontSize: '1.125rem',
+      fontWeigth: 'bold',
+      lineHeigth: 1.6,
+    },
+    a: {
+      textDecorationm: 'none',
+      cursor: 'pointer',
+      color: '$green500',
+      fontWeight: 'bold',
+      transition: 'color 0.3s',
+
+      '&:hover': {
+        color: '$green300',
+      },
+    },
+  },
+});
+
+export const FooterPopup = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 10,
+  div: {
+    display: 'flex',
+    justifyContent: 'space-between',
+
+    strong: {
+      fontSize: '1.125rem',
+    },
+  },
+
+   button: {
+    marginTop: 40,
+    backgroundColor: '$green500',
+    border: 0,
+    color: '$white',
+    borderRadius: 8,
+    padding: '1.25rem',
+    cursor: 'pointer',
+    fontWeight: 'bold',
+    transition: '0.3s',
+    '&:not(:disabled):hover': {
+      backgroundColor: '$green300',
+    },
+
+    '&:disabled': {
+      opacity: 0.6,
+      cursor: "not-allowed"
+    }
+  },
+});
